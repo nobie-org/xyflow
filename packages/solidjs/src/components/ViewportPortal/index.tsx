14c57e45c69174c5d34b046a1cb597a2f142ee5e
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2024-04-28 12:43:56 -0700

    wip

--- packages/solidjs/src/components/ViewportPortal/index.tsx
+++ packages/solidjs/src/components/ViewportPortal/index.tsx
@@ -1,17 +1,26 @@
-import type { ReactNode } from 'react';
-import { createPortal } from 'react-dom';
+// import type { ReactNode } from 'react';
+// import { createPortal } from 'react-dom';
 
 import { useStore } from '../../hooks/useStore';
 import type { SolidFlowState } from '../../types';
+import { ParentProps, Show } from 'solid-js';
+import { Portal } from 'solid-js/web';
 
-const selector = (s: SolidFlowState) => s.domNode?.querySelector('.react-flow__viewport-portal');
+const selector = (s: SolidFlowState) => () => s.domNode.get()?.querySelector('.react-flow__viewport-portal');
 
-export function ViewportPortal({ children }: { children: ReactNode }) {
+export function ViewportPortal(p: ParentProps) {
   const viewPortalDiv = useStore(selector);
 
-  if (!viewPortalDiv) {
-    return null;
-  }
+  // if (!viewPortalDiv) {
+  //   return null;
+  // }
+  return (
+    <Show when={viewPortalDiv()}>
+      {(el) => {
+        return <Portal mount={el()}>{p.children}</Portal>;
+      }}
+    </Show>
+  );
 
-  return createPortal(children, viewPortalDiv);
+  // return createPortal(children, viewPortalDiv);
 }
