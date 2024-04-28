535eafea87a37a50ed3e441c31f7ccb60d2ed072
Matthew Gapp <61894094+matthewgapp@users.noreply.github.com>
2024-04-28 10:18:52 -0700

    wip

--- packages/solidjs/src/components/EdgeLabelRenderer/index.tsx
+++ packages/solidjs/src/components/EdgeLabelRenderer/index.tsx
@@ -1,17 +1,23 @@
 import type { ReactNode } from 'react';
-import { createPortal } from 'react-dom';
+// import { createPortal } from 'react-dom';
 
 import { useStore } from '../../hooks/useStore';
 import type { SolidFlowState } from '../../types';
+import { Portal } from 'solid-js/web'
+import { ParentProps } from 'solid-js';
 
-const selector = (s: SolidFlowState) => s.domNode?.querySelector('.react-flow__edgelabel-renderer');
+const selector = (s: SolidFlowState) => () => s.domNode.get()?.querySelector('.react-flow__edgelabel-renderer');
 
-export function EdgeLabelRenderer({ children }: { children: ReactNode }) {
+export function EdgeLabelRenderer(p: ParentProps)) {
   const edgeLabelRenderer = useStore(selector);
 
   if (!edgeLabelRenderer) {
     return null;
   }
 
+  return <Portal mount={edgeLabelRenderer()}>
+    {p.children}
+
+  </Portal>
   return createPortal(children, edgeLabelRenderer);
 }
